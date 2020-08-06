9461b0dbf17c7fc045ba6450b46a89669189c487
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-08-06 00:25:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function moveDodgerRight() {
   let rightNumbers = dodger.style.right.replace("px", "");
   let right = parseInt(rightNumbers, 10);
  
-  if (right > 0) {
+  if (right < 180) {
     dodger.style.right = `${right + 1}px`;
   }
 }
