9cfc363734f90f4faeb0377361a6f8a372480baa
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-08-06 00:18:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,18 @@ document.addEventListener("keydown", function(e) {
     moveDodgerLeft();
   }
 });
+
+function moveDodgerRight() {
+  let rightNumbers = dodger.style.right.replace("px", "");
+  let right = parseInt(rightNumbers, 10);
+ 
+  if (right < 180) {
+    dodger.style.right = `${right + 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
